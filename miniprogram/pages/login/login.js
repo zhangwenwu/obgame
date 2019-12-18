c9211dfa4a6755cf02eb212c8d1d1a7c3dece68a
zhangwenwu <769373420>
2019-12-18 09:55:19 +0800

    欢迎页

--- miniprogram/pages/login/login.js
+++ miniprogram/pages/login/login.js
@@ -8,36 +8,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // wx.getSetting({
-    //   success(res) {
-    //     if (res.authSetting['scope.userInfo']) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-    //       wx.getUserInfo({
-    //         success: function (res) {
-    //           this.
-    //           wx.navigateTo({
-    //             url: '/pages/index/index'
-    //           })
-    //         }
-    //       })
-    //     }
-    //   }
-    // })
+     let _this = this
+     wx.getSetting({
+       success(res) {
+         if (res.authSetting['scope.userInfo']) {
+           // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+           wx.getUserInfo({
+             success: function (res) {
+              _this.setData({
+                canIUse: false,
+              })
+               wx.navigateTo({
+                 url: '/pages/index/index'
+               })
+             }
+           })
+         }else{
+            console.log("未授权")
+            _this.setData({
+              canIUse: true,
+            })
+          }
+       }
+     })
   },
 
   bindGetUserInfo(e) {
     if (e.detail.rawData) {
       console.log("onGetUserInfo", e.detail)
       wx.setStorageSync("userInfo", e.detail.rawData)
+      this.setData({
+        canIUse: false,
+      })
       wx.navigateTo({
-        url: '/pages/index/index'
+      url: '/pages/index/index'
       })
     }
   }
